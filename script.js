da5856b2bdde983fff9f79ef57e3a6ece86c9a35
Jasmin Dedic <jdedic1999@gmail.com>
2022-08-27 09:52:09 +0200

    Finished project

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let panels = document.querySelectorAll(".panel");
+
+
+// Event listener to change active class
+for (let i = 0; i < panels.length; i++){
+    panels[i].addEventListener("click", () => {
+        removeActiveClass();
+        panels[i].classList.add("active");
+    })
+}
+
+
+// Function to remove active class
+function removeActiveClass () {
+    for (let i = 0; i < panels.length; i++) {
+        panels[i].classList.remove("active");
+    }
+};
